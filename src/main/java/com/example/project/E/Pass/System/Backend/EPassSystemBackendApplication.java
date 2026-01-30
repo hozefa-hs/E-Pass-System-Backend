79d399f170c2fa66f9c5671c323dd71610547875
Hozefa <hozefasailanawala786@gmail.com>
2026-01-31 00:30:45 +0530

    Initial Commit

---
+++ src/main/java/com/example/project/E/Pass/System/Backend/EPassSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.E.Pass.System.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPassSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPassSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project/E/Pass/System/Backend/EPassSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.E.Pass.System.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPassSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
